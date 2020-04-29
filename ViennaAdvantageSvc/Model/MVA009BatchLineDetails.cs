826a489d476a5865a081dd6bc4c90d90d9b6fcfe
manjot.singh <manjot.singh@viennasolutions.com>
2020-04-29 13:09:47 +0530

    Did changes given by Ashish and Rajni Regarding Batch Generation

--- ViennaAdvantageSvc/Model/MVA009BatchLineDetails.cs
+++ ViennaAdvantageSvc/Model/MVA009BatchLineDetails.cs
@@ -29,5 +29,20 @@ namespace ViennaAdvantage.Model
             DB.ExecuteQuery(sql, null, Get_Trx());
             return true;
         }
+        /// <summary>
+        /// to implement delete functionality 
+        /// </summary>
+        /// <returns>flag whether deletion possible or not</returns>
+        protected override bool BeforeDelete()
+        {
+            int count = Util.GetValueOfInt(DB.ExecuteScalar(@"SELECT COUNT(*)
+                        FROM va009_batchlinedetails WHERE va009_batchlines_id = " + GetVA009_BatchLines_ID() + " AND processed = 'Y' "));
+            if (count > 0)
+            {
+                return false;
+            }
+            return true;
+
+        }
     }
 }
