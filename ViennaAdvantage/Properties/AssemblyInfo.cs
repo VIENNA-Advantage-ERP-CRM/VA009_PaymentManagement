6204294c6568704e1d5a0b6898255ed7927fdc69
prelivemoduleserver <prelivemoduleserver@viennasolutions.com>
2022-06-02 10:39:39 +0000

    1.1.6.1

--- ViennaAdvantage/Properties/AssemblyInfo.cs
+++ ViennaAdvantage/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.6.5")] // Do not alter or change this version
+[assembly: AssemblyVersion("1.0.6.5")] /// Do not alter or change this version
 [assembly: AssemblyFileVersion("1.1.2.2")]
