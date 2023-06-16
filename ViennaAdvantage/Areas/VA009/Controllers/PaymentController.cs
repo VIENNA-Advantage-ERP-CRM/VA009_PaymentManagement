01415ef19a4690d0c3e1db63fc069257542d9863
neha.kumari <neha.kumari@ViennaSolutions.Com>
2023-06-16 13:56:03 +0530

    Get journal details

--- ViennaAdvantage/Areas/VA009/Controllers/PaymentController.cs
+++ ViennaAdvantage/Areas/VA009/Controllers/PaymentController.cs
@@ -444,11 +444,18 @@ namespace VA009.Controllers
         /// </summary>
         /// <param name="BP"></param>
         /// <returns></returns>
-        public ActionResult GetLocation(string BP)
+        //public ActionResult GetLocation(string BP)
+        //{
+        //    Ctx ct = Session["ctx"] as Ctx;
+        //    PaymentModel _payMdl = new PaymentModel();
+        //    List<LocationDetails> _LocationData = _payMdl.GetLocation(BP, ct);
+        //    return Json(JsonConvert.SerializeObject(_LocationData), JsonRequestBehavior.AllowGet);
+        //}
+        public ActionResult GetLocation(string BP, string locationId)
         {
             Ctx ct = Session["ctx"] as Ctx;
             PaymentModel _payMdl = new PaymentModel();
-            List<LocationDetails> _LocationData = _payMdl.GetLocation(BP, ct);
+            List<LocationDetails> _LocationData = _payMdl.GetLocation(BP,ct, locationId);
             return Json(JsonConvert.SerializeObject(_LocationData), JsonRequestBehavior.AllowGet);
         }
         /// <summary>
